12c6e2c326ddedc16e4114783c55675f15c26fd9
THATBLADEBOY <aus10b@gmail.com>
2019-01-28 13:45:59 -0600

    added Events link in navbar; created a modules folder; set state in ApplicationViews.js

---
+++ src/modules/EventManger.js
@@ -0,0 +1,19 @@
+const remoteURL = "http://localhost:5002";
+
+export default {
+  get(id) {
+    return fetch(`${remoteURL}/events/${id}`).then(e => e.json());
+  },
+  getAll() {
+    return fetch(`${remoteURL}/events`).then(e => e.json());
+  },
+  post(newEvent) {
+    return fetch(`${remoteURL}/events`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newEvent)
+    }).then(data => data.json());
+  }
+};
