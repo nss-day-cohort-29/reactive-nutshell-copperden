49e2e5d83a02aaa7f14f124312fdad11105778c1
THATBLADEBOY <aus10b@gmail.com>
2019-01-30 14:27:12 -0600

    committing to merge with master

--- src/components/friends/Friends.js
+++ src/components/friends/Friends.js
@@ -3,16 +3,26 @@ import React, { Component } from 'react'
 export class Friends extends Component {
   render() {
 
-    let currentUser = sessionStorage.getItem("credentials");
-    console.log(currentUser);
+    // let currentUser = sessionStorage.getItem("credentials");
+    // // console.log(currentUser);
+
+    this.props.friends.map(friend => {
+      console.log(friend.userId)
+    })
+
+
+    // this.props.friends.map(friend => {
+    //   if(friend.currentUserId === currentUser) {
+    //     console.log(friend)}
+    //   }
+      
+
     return (
       <div>
           <button className="btn">Add Friends</button>
           <div className="current-friends-div">
             <h2>Your Friends</h2>
-            {this.props.friends.map(friend => {
-                console.log(friend.id)
-            })}
+            
           
           </div>
         
