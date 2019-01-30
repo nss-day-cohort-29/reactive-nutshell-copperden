92626f580eea5f878e73b62e7a0d50848c4b6571
Hunter Metts <huntermettsdev@gmail.com>
2019-01-30 12:26:42 -0600

    login and setting session storage

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -23,18 +23,22 @@ export default class Login extends Component {
         e.preventDefault()
 
     //    Setting username in session storage. Grabbing the username from session storage and searching through "users" in the datatbase. The .find attempts to find a username that matches the username in session storage. If able to find a match, log in under that user. If not, display message that username not found.
-       
+
         sessionStorage.setItem(
             "username",
             this.state.username)
-        
+
         let currentUser = sessionStorage.getItem("username")
-        let authenticated = this.props.users.find(user => 
+        let authenticated = this.props.users.find(user =>
             user.name === currentUser)
-        
-            
+
+
             console.log(authenticated)
-        this.props.history.push("/")
+            if (authenticated === undefined){
+                alert("Whoops! We we couldn't find your account. Please re-renter a valid username and email or sign up below!")
+                window.location.reload()
+                // this.props.history.push("/register")
+            } else (this.props.history.push("/"))
     }
 
     render() {
